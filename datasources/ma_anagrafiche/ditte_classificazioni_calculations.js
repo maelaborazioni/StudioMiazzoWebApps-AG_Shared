2fc5984b49993a7d3673f47dc40070956d1bc328
Giovanni Tizzoni <giovanni.tizzoni@gmail.com>
2018-06-25 15:08:16 +0200

    New supporting files
    [ADD] Relation lavoratori_classificazioni_to_ditte_classificazioni
    [ADD] Relation ditte_legami_to_ditte_classificazioni

--- datasources/ma_anagrafiche/ditte_classificazioni_calculations.js
+++ datasources/ma_anagrafiche/ditte_classificazioni_calculations.js
@@ -1,4 +1,12 @@
 /**
+ * @properties={type:4,typeid:36,uuid:"0905CC07-DB97-4C33-B912-3908DDDD63D0"}
+ */
+function codice_int()
+{
+	return codice && parseInt(codice,10);
+}
+
+/**
  * @properties={type:12,typeid:36,uuid:"7295CF13-0589-4715-9170-86A806167514"}
  */
 function descrizione_calc()
